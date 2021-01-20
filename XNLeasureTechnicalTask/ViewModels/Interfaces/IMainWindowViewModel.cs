b06c82ebfc3457219d2655368a756e8793379f3e
ROBSEPICGAMINGB\Rob <thundergod6115@hotmail.com>
2021-01-20 21:24:19 +0000

    Add ViewModels

---
+++ XNLeasureTechnicalTask/ViewModels/Interfaces/IMainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XNLeasureTechnicalTask.ViewModels.Interfaces
+{
+    public interface IMainWindowViewModel
+    {
+        string StringInput { get; set; }
+        char CharToCount { get; set; }
+        int CharCount { get; set; }
+        bool IgnoreCase { get; set; }
+        string CharCountResult { get; set; }
+        string FirstNumber { get; set; }
+        string SecondNumber { get; set; }
+        string MathResult { get; set; }
+    }
+}
--- XNLeasureTechnicalTask/ViewModels/MainWindowViewModel.cs
+++ XNLeasureTechnicalTask/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Prism.Commands;
 using System.Collections.Generic;
-using System.Text;
-using XNLeasureTechnicalTask.Services;
+using System.Windows.Input;
 using XNLeasureTechnicalTask.Services.Interfaces;
+using XNLeasureTechnicalTask.ViewModels.Interfaces;
 
 namespace XNLeasureTechnicalTask.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModelBase, IMainWindowViewModel
     {
+        private string _stringInput;
+        private char _charToCount;
+        private int _charCount;
+        private bool _ignoreCase;
+        private string _charCountResult;
+        private string _firstNumber;
+        private string _secondNumber;
+        private string _mathResult;
+
         private readonly IMathService _mathService;
         private readonly ICharacterCountService _characterCountService;
 
@@ -15,6 +24,117 @@ namespace XNLeasureTechnicalTask.ViewModels
         {
             _mathService = mathService;
             _characterCountService = characterCountService;
+
+            AddCommand = new DelegateCommand(Add);
+            CountCommand = new DelegateCommand(Count);
+        }
+
+        #region properties
+
+        public string StringInput
+        {
+            get { return _stringInput; }
+            set
+            {
+                _stringInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public char CharToCount
+        {
+            get { return _charToCount; }
+            set
+            {
+                _charToCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int CharCount
+        {
+            get { return _charCount; }
+            set
+            {
+                _charCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IgnoreCase
+        {
+            get { return _ignoreCase; }
+            set
+            {
+                _ignoreCase = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string CharCountResult
+        {
+            get { return _charCountResult; }
+            set
+            {
+                _charCountResult = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstNumber
+        {
+            get { return _firstNumber; }
+            set
+            {
+                _firstNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SecondNumber
+        {
+            get { return _secondNumber; }
+            set
+            {
+                _secondNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string MathResult
+        {
+            get { return _mathResult; }
+            set
+            {
+                _mathResult = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CountCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+
+        #endregion properties
+
+        private void Count()
+        {
+            CharCount = _characterCountService.GetNoOfCharacters(StringInput, CharToCount, IgnoreCase);
+
+            CharCountResult = $"{CharCount} occurences of {CharToCount}";
+        }
+
+        private void Add()
+        {
+            double first;
+            double second;
+            if (double.TryParse(FirstNumber, out first) == true && double.TryParse(SecondNumber, out second) == true)
+            {
+                MathResult = _mathService.AddNumbers(new List<double> { first, second }).ToString();
+            }
+            else
+            {
+                MathResult = "Syntax Error";
+            }
         }
     }
 }
